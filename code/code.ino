255a1ef2e74a38ce90b60f14ba4973b125259f5d
AL <alexander@levineportfolio.com>
2025-07-22 19:09:59 -0500

    semicolons

--- code/code.ino
+++ code/code.ino
@@ -10,14 +10,14 @@ Al Levine
 
 // ~~~~~~~~~~~~~~~~~~~~ HARDWARE PIN DEFINITIONS ~~~~~~~~~~~~~~~~~~~~
 // use const instead of #define, best practice according to arduino documentation
-const SCL A5  // i2c SCL
-const SDA A4  // i2c SDA
-const DIN 4   // HV8512 serial data input for 1st chip
-const BLK 6   // HV5812 blanking input
-const CLK 7   // HV5812 data shift register clock input
-const STR 8   // HV5812 output enable input
-const DIM 9   // Dimming PWM
-const POT A7  // Potentiometer input
+const SCL A5;  // i2c SCL
+const SDA A4;  // i2c SDA
+const DIN 4;   // HV8512 serial data input for 1st chip
+const BLK 6;   // HV5812 blanking input
+const CLK 7;   // HV5812 data shift register clock input
+const STR 8;   // HV5812 output enable input
+const DIM 9;   // Dimming PWM
+const POT A7;  // Potentiometer input
 
 // Assignment of tube cathodes to HV8512 driver pins
 /*
@@ -40,21 +40,21 @@ int tube4[7] = { 17, 16, 15, 14, 13, 12, 11 };  // add 20 for tube 2 second chip
 
 // ~~~~~~~~~~~~~~~~~~~~ MISC SETUP ~~~~~~~~~~~~~~~~~~~~
 
-boolean nixieDisplayArray[40]; // Bit array for 2x HV5812 chips
-int digits[4]={0,0,0,0}; // array to store digits
+boolean nixieDisplayArray[40];   // Bit array for 2x HV5812 chips
+int digits[4] = { 0, 0, 0, 0 };  // array to store digits
 
 // Font table
 //	tube pin   1,5,6,7,8,9,10
-int num0[] = {1,1,1,1,1,0,1};
-int num1[] = {0,0,1,1,0,0,0};
-int num2[] = {1,1,0,1,1,0,0};
-int num3[] = {0,1,1,1,1,1,0};
-int num4[] = {0,0,1,1,0,1,1};
-int num5[] = {0,1,1,0,1,1,1};
-int num6[] = {1,1,1,0,1,1,1};
-int num7[] = {0,0,1,1,1,0,0};
-int num8[] = {1,1,1,1,1,1,1};
-int num9[] = {0,1,1,1,1,1,1};
+int num0[] = { 1, 1, 1, 1, 1, 0, 1 };
+int num1[] = { 0, 0, 1, 1, 0, 0, 0 };
+int num2[] = { 1, 1, 0, 1, 1, 0, 0 };
+int num3[] = { 0, 1, 1, 1, 1, 1, 0 };
+int num4[] = { 0, 0, 1, 1, 0, 1, 1 };
+int num5[] = { 0, 1, 1, 0, 1, 1, 1 };
+int num6[] = { 1, 1, 1, 0, 1, 1, 1 };
+int num7[] = { 0, 0, 1, 1, 1, 0, 0 };
+int num8[] = { 1, 1, 1, 1, 1, 1, 1 };
+int num9[] = { 0, 1, 1, 1, 1, 1, 1 };
 
 
 // ~~~~~~~~~~~~~~~~~~~~ SETUP FUNCTION ~~~~~~~~~~~~~~~~~~~~
@@ -79,18 +79,18 @@ void loop() {
 }
 
 // ~~~~~~~~~~~~~~~~~~~~ OTHER FUNCTIONS ~~~~~~~~~~~~~~~~~~~~
-int splitNum(int num){ // split 4 digit integer into array of digits
-	int array{4};
+int splitNum(int num) {  // split 4 digit integer into array of digits
+	int array{ 4 };
 	array[0] = (number / 1) % 10;
-    array[0] = (number / 10) % 10;
-    array[0] = (number / 100) % 10;
-    array[0] = (number / 1000) % 10;
+	array[0] = (number / 10) % 10;
+	array[0] = (number / 100) % 10;
+	array[0] = (number / 1000) % 10;
 	return array[];
 
-int printNum(int buffer, int tube, int num) { //writes font table into display buffer, one tube at a time
-	for(int i=0; i<7; i++){
-		int pos = tube[i];  // get pin number from tube pin assignment array
-		buffer[pos] = num[i]; // write boolean value from font table to relevant pin in display buffer
+	int printNum(int buffer, int tube, int num) {  //writes font table into display buffer, one tube at a time
+		for (int i = 0; i < 7; i++) {
+			int pos = tube[i];     // get pin number from tube pin assignment array
+			buffer[pos] = num[i];  // write boolean value from font table to relevant pin in display buffer
+		}
+		return buffer;
 	}
-	return buffer;
-}
